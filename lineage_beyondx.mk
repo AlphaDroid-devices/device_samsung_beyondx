2a742a8b06728050e964fa78c9600ce82b782a44
Nathan Moore <124376269+Mgm2023@users.noreply.github.com>
2023-06-06 09:59:36 +0100

    beyondx: lineage_beyondx: update viper fx paths

--- lineage_beyondx.mk
+++ lineage_beyondx.mk
@@ -28,7 +28,8 @@ TARGET_HAVE_FOD := true
 ## Inherit from beyondx device
 $(call inherit-product, device/samsung/beyondx/device.mk)
 
-$(call inherit-product, device/samsung/exynos9820-common/v4afx/config.mk)
+## Inherit v4afx
+$(call inherit-product, vendor/v4afx/config.mk)
 
 WITH_GAPPS := 0
 # Valid for core/full GAPPS (default=true)
